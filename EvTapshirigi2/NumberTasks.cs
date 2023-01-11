fec39c8a882bab6527e0385264aa28b9769c03e4
kenanxasiyevv <kananakh@code.edu.az>
2023-01-12 03:51:05 +0400

    Add project files.

---
+++ EvTapshirigi2/NumberTasks.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvTapshirigi2
+{
+    internal class NumberTasks
+    {
+        internal void Print()
+        {
+            int reversenumber = ReverseNumber();
+            Console.Write(reversenumber);
+        }
+        internal int ReverseNumber()
+        {
+            Console.Write("Eded daxil edin:");
+            int number = int.Parse(Console.ReadLine());
+            int count = (int)Math.Log10(number);
+            int r;
+            int reversenumber = 0;
+
+            while (number != 0)
+            {
+                r = number % 10;
+                number /= 10;
+
+                reversenumber += r * (int)Math.Pow(10, count);
+                count--;
+            }
+            return reversenumber;
+        }
+
+    }
+}
---
+++ EvTapshirigi2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace EvTapshirigi2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbertasks = new NumberTasks();
+
+            var reversenumber = numbertasks.ReverseNumber;
+
+            numbertasks.Print();
+        }
+    }
+}
