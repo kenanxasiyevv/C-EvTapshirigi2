a09bed50578b4307513b4b34274c5782f1fb1e7c
Kanan Xasiyev <121182410+kenanxasiyevv@users.noreply.github.com>
2023-01-12 19:52:20 +0400

    Update NumberTasks.cs

--- EvTapshirigi2/NumberTasks.cs
+++ EvTapshirigi2/NumberTasks.cs
@@ -9,29 +9,48 @@ namespace EvTapshirigi2
 {
     internal class NumberTasks
     {
-        internal void Print()
+         internal int[] EnterArray()
         {
-            int reversenumber = ReverseNumber();
-            Console.Write(reversenumber);
-        }
-        internal int ReverseNumber()
-        {
-            Console.Write("Eded daxil edin:");
-            int number = int.Parse(Console.ReadLine());
-            int count = (int)Math.Log10(number);
-            int r;
-            int reversenumber = 0;
+            Console.Write("Massivin uzunlugunu daxil et: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] mass = new int[n];
 
-            while (number != 0)
+            for (int i = 0; i < mass.Length; i++)
             {
-                r = number % 10;
-                number /= 10;
-
-                reversenumber += r * (int)Math.Pow(10, count);
-                count--;
+                Console.Write($"mass[{i}] = ");
+                mass[i] = int.Parse(Console.ReadLine());
             }
-            return reversenumber;
+            return mass;
         }
 
+
+        internal int[] SquareRootElements(int[] mass)
+        {
+            int count = 0;
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (Math.Sqrt(mass[i]) == (int)Math.Sqrt(mass[i]))
+                    count++;
+            }
+            
+            int[] squarerootElements = new int[count];
+            int index = 0;
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (Math.Sqrt(mass[i]) == (int)Math.Sqrt(mass[i]))
+                    squarerootElements[index++] = mass[i];
+            }
+            return squarerootElements;
+        }
+
+        internal void Print(int[] mass)
+        {
+            foreach (var item in mass)
+            {
+                Console.Write(item + " ");
+            }
+        }
     }
 }
